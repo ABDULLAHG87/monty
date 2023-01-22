92b1e7384a66404d5daf1fcc2629a42db849aa5c
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2023-01-22 14:40:46 -0800

    created monty executive code file

--- monty_run.c
+++ monty_run.c
@@ -1,169 +1,54 @@
 #include "monty.h"
-#include <string.h>
-
-void free_tokens(void);
-unsigned int token_arr_len(void);
-int is_empty_line(char *line, char *delims);
-void (*get_op_func(char *opcode))(stack_t**, unsigned int);
-int run_monty(FILE *script_fd);
 
 /**
- * free_tokens - Frees the global op_toks array of strings.
- */
-void free_tokens(void)
+* execute - function that executes the opcode
+* @stack: head stack linked list
+* @counter: line count
+* @file: pointer to monty file stream
+* @content: line content
+*
+* Return: nothing
+*/
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
-	size_t i = 0;
+	instruction_t opst[] = {
+				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+				{"pop", f_pop},
+				{"swap", f_swap},
+				{"add", f_add},
+				{"nop", f_nop},
+				{"sub", f_sub},
+				{"div", f_div},
+				{"mul", f_mul},
+				{"mod", f_mod},
+				{"pchar", f_pchar},
+				{"pstr", f_pstr},
+				{"rotl", f_rotl},
+				{"rotr", f_rotr},
+				{"queue", f_queue},
+				{"stack", f_stack},
+				{NULL, NULL}
+				};
+	unsigned int i = 0;
+	char *op;
 
-	if (op_toks == NULL)
-		return;
-
-	for (i = 0; op_toks[i]; i++)
-		free(op_toks[i]);
-
-	free(op_toks);
-}
-
-/**
- * token_arr_len - Gets the length of current op_toks.
- *
- * Return: Length of current op_toks (as int).
- */
-unsigned int token_arr_len(void)
-{
-	unsigned int toks_len = 0;
-
-	while (op_toks[toks_len])
-		toks_len++;
-	return (toks_len);
-}
-
-/**
- * is_empty_line - Checks if a line read from getline only contains delimiters.
- * @line: A pointer to the line.
- * @delims: A string of delimiter characters.
- *
- * Return: If the line only contains delimiters - 1.
- *         Otherwise - 0.
- */
-int is_empty_line(char *line, char *delims)
-{
-	int i, j;
-
-	for (i = 0; line[i]; i++)
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
 	{
-		for (j = 0; delims[j]; j++)
-		{
-			if (line[i] == delims[j])
-				break;
-		}
-		if (delims[j] == '\0')
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
 			return (0);
+		}
+		i++;
 	}
-
+	if (op && opst[i].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); }
 	return (1);
-}
-
-/**
- * get_op_func - Matches an opcode with its corresponding function.
- * @opcode: The opcode to match.
- *
- * Return: A pointer to the corresponding function.
- */
-void (*get_op_func(char *opcode))(stack_t**, unsigned int)
-{
-	instruction_t op_funcs[] = {
-		{"push", monty_push},
-		{"pall", monty_pall},
-		{"pint", monty_pint},
-		{"pop", monty_pop},
-		{"swap", monty_swap},
-		{"add", monty_add},
-		{"nop", monty_nop},
-		{"sub", monty_sub},
-		{"div", monty_div},
-		{"mul", monty_mul},
-		{"mod", monty_mod},
-		{"pchar", monty_pchar},
-		{"pstr", monty_pstr},
-		{"rotl", monty_rotl},
-		{"rotr", monty_rotr},
-		{"stack", monty_stack},
-		{"queue", monty_queue},
-		{NULL, NULL}
-	};
-	int i;
-
-	for (i = 0; op_funcs[i].opcode; i++)
-	{
-		if (strcmp(opcode, op_funcs[i].opcode) == 0)
-			return (op_funcs[i].f);
-	}
-
-	return (NULL);
-}
-
-/**
- * run_monty - Primary function to execute a Monty bytecodes script.
- * @script_fd: File descriptor for an open Monty bytecodes script.
- *
- * Return: EXIT_SUCCESS on success, respective error code on failure.
- */
-int run_monty(FILE *script_fd)
-{
-	stack_t *stack = NULL;
-	char *line = NULL;
-	size_t len = 0, exit_status = EXIT_SUCCESS;
-	unsigned int line_number = 0, prev_tok_len = 0;
-	void (*op_func)(stack_t**, unsigned int);
-
-	if (init_stack(&stack) == EXIT_FAILURE)
-		return (EXIT_FAILURE);
-
-	while (getline(&line, &len, script_fd) != -1)
-	{
-		line_number++;
-		op_toks = strtow(line, DELIMS);
-		if (op_toks == NULL)
-		{
-			if (is_empty_line(line, DELIMS))
-				continue;
-			free_stack(&stack);
-			return (malloc_error());
-		}
-		else if (op_toks[0][0] == '#') /* comment line */
-		{
-			free_tokens();
-			continue;
-		}
-		op_func = get_op_func(op_toks[0]);
-		if (op_func == NULL)
-		{
-			free_stack(&stack);
-			exit_status = unknown_op_error(op_toks[0], line_number);
-			free_tokens();
-			break;
-		}
-		prev_tok_len = token_arr_len();
-		op_func(&stack, line_number);
-		if (token_arr_len() != prev_tok_len)
-		{
-			if (op_toks && op_toks[prev_tok_len])
-				exit_status = atoi(op_toks[prev_tok_len]);
-			else
-				exit_status = EXIT_FAILURE;
-			free_tokens();
-			break;
-		}
-		free_tokens();
-	}
-	free_stack(&stack);
-
-	if (line && *line == 0)
-	{
-		free(line);
-		return (malloc_error());
-	}
-
-	free(line);
-	return (exit_status);
 }
