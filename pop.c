411978fd0f105206850a5958a8f0eba40029c903
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2023-01-22 15:08:57 -0800

    created the pop function file

---
+++ pop.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+* f_pop - function that prints the top of the stack
+* @head: double head pointer to the stack
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_pop(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
+}
